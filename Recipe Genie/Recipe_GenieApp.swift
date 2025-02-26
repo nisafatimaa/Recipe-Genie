28c73eb2dda3a0473aac39b63df08bb50713faed
Nisa Fatima <nisafatima0000@gmail.com>
2025-02-26 20:38:07 +0500

    Initial Commit

---
+++ Recipe Genie/Recipe_GenieApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_GenieApp.swift
+//  Recipe Genie
+//
+//  Created by Nisa on 26/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_GenieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
